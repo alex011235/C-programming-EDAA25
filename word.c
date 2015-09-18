8843779ac4c4e831d40f16425a1938f58bbfe64e
alex011235 <st.alexander85@gmail.com>
2015-09-18 17:02:10 +0200

    better

--- word.c
+++ word.c
@@ -1,64 +1,38 @@
 /* Word-assignment, Alexander Karlsson */
 #include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-char* word_array;
-char* temp_array;
-int size = 8;
-int longest = 0;
-
-void init()
+int main(void)
 {
-	word_array = (char *) malloc(size*sizeof(char));
-	temp_array = (char *) malloc(size*sizeof(char));
-}
+	int size = 128;
+	char* word_array = (char *) malloc(size*sizeof(char));
+	char* temp_array = (char *) malloc(size*sizeof(char));
 
-/* Resize and copy if bigger word is found */
-void resize(int len)
-{
-	size *= 2;
-	word_array = (char *) realloc(word_array,size);
-	strncpy(word_array,temp_array,size/2);
-	temp_array = (char *) realloc(temp_array,size);
-	strncpy(temp_array,word_array,size/2);
-}
-
-void find_longest_word()
-{
-
-	int temp = 0;
+	int temp_count = 0, longest = 0;
 	int x;
-	word_array[0] = '\0';
-	temp_array[0] = '\0';
 
 	while ((x = getchar()) != EOF) {
 
-		if (!isalpha(x)) {
-			if (temp > longest) {
-				longest = temp;
-				resize(temp);
-			}
-			temp = 0;
-
-		}
 		if (isalpha(x)) {
-			if (temp == size) {
-				resize(temp);
+			if (temp_count >= size) {
+				size *= 2;
+				temp_array = (char *) realloc(temp_array,size);
 			}
-			temp_array[temp++] = x;
+			temp_array[temp_count++] = x;
+		}
+
+		if (!isalpha(x)){
+			if (temp_count > longest) {
+				longest = temp_count;
+				word_array = (char *) realloc(word_array,size);
+				strncpy(word_array,temp_array,size);
+			}
+			temp_count = 0;
 		}
 	}
 
 	printf("%d characters in longest word: %s\n",longest,word_array);
-
-}
-
-int main(void)
-{
-	init();
-	find_longest_word();
 	free(word_array);
 	free(temp_array);
 	return 0;
